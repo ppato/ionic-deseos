47bdc233d22950a386f718e756d506ff59b60b35
ppato <pizarroarancibia@gmail.com>
2020-07-25 14:26:52 -0400

    version 1

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {Lista}  from '../models/lista.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+  listas :Lista []=[];
+
+
+  constructor() {
+//console.log('servicio inicializado')
+this.cargarStorage();
+//const lista1 = new Lista('recolectar piedras del infinito');
+//const lista2 = new Lista('heroes a desaparecer');
+
+//this.listas.push(lista1,lista2);
+
+//console.log('this.listas');
+   }
+
+
+crearLista(titulo:string){
+  const nuevaLista = new Lista(titulo);
+  this.listas.push(nuevaLista);
+  this.guardarStorage();
+
+  return nuevaLista.id;
+}
+
+obtenerLista(id:string | number){
+  id=Number(id);
+  return  this.listas.find(listaData=>listaData.id===id);
+
+}
+
+
+guardarStorage(){
+localStorage.setItem('data',JSON.stringify(this.listas));
+}
+
+cargarStorage(){
+  if (localStorage.getItem('data')){
+this.listas=JSON.parse(localStorage.getItem('data'));
+}
+else{
+  this.listas=[];
+}
+
+}
+
+
+}
